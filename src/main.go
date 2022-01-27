62a45805a58dc140f144250f2ebc3e49e294730a
drull1000 <pcaetanop@protonmail.com>
2022-01-27 18:14:52 -0300

    Removing logs

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -24,9 +23,6 @@ func main() {
 			if update.Message.Text == "/start" {
 				for {
 					a.FetchNews(update)
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "end")
-					bot.Send(msg)
-					fmt.Println("I sleep")
 					time.Sleep(time.Hour)
 				}
 			}
